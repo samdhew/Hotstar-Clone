181110b0e45232dee9541029f194bbdcc998d253
Sampath Hewage <samdhew@users.noreply.github.com>
2024-02-03 20:43:56 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "3pl-dev-accseg-backups" # Replace with your actual S3 bucket name
+    key    = "sampoc/EKS/terraform.tfstate"
+    region = "us-east-1"
   }
 }
